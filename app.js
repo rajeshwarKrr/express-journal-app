6fcd7d39a0274eff1fa4c0181f5759b3f468d4a9
Rajeshwar Reddy <rajeshwarreddy.kadari@gmail.com>
2019-08-21 23:26:41 +0530

    fixed cors

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
-
+const cors = require("cors");
 const app = express();
 
 const dbconfig = require("./routes/config/database.config.js");
-
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
